eac06bcb04f88abadd3e6aa7ca37df93151e50f3
JCruz6725 <john.cruz6725@gmail.com>
2025-06-12 11:22:35 -0500

    Extend testcase object

--- FaultyPalindromeChecker/TestUtility/TestCase.cs
+++ FaultyPalindromeChecker/TestUtility/TestCase.cs
@@ -4,5 +4,11 @@
         public required bool Expected { get; set; }
         public bool? Results { get; set; } = null;
         public Exception? error { get; set; } = null;
+
+        public bool IsValid() {
+            if(Results is null)
+                return false;
+            return Results == Expected;
+        }
     }
 }
